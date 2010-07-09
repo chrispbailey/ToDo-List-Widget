061a94e3c10d2ce53eaf9f5078a7347ae41b2b2c
Chris Bailey <c.bailey@bristol.ac.uk>
2010-07-09 22:33:18 +0100

    catching npe from crash report

--- src/org/chrisbailey/todo/ToDoWidgetProvider.java
+++ src/org/chrisbailey/todo/ToDoWidgetProvider.java
@@ -69,78 +69,85 @@ public class ToDoWidgetProvider extends AppWidgetProvider
 
     public static void updateAppWidget(Context context, AppWidgetManager manager, int appWidgetId)
     {
-        ToDoDatabase db = new ToDoDatabase(context.getApplicationContext());
-        PreferenceManager pm = new PreferenceManager(context, db);
-        RemoteViews views = new RemoteViews(context.getPackageName(), manager.getAppWidgetInfo(appWidgetId).initialLayout);
-
-        views.setImageViewResource(R.id.widget_background, pm.getBackground());
-
-        // set the note title
-        String title = db.getTitle(appWidgetId);
-        title.trim();
-        views.setTextViewText(R.id.notetitle, Html.fromHtml("<b><u>"+title+"</u></b>"));
-        views.setViewVisibility(R.id.notetitle, View.VISIBLE);
-        views.setTextColor(R.id.notetitle, pm.getActiveColor());
-        views.setFloat(R.id.notetitle, "setTextSize", pm.getTitleSize());
-        
-        if (title.length() == 0)
+        try
         {
-            views.setViewVisibility(R.id.notetitle, View.GONE);
-        }
-        
-        // set the note items
-        LinkedList<Note> notes = db.getAllNotes(appWidgetId);
-        
-        db.close();
-        db = null;
-
-        int noteField;
-        int imageField;
-        
-        for (int i=0; i< MAX_NOTES; i++)
-        {
-            try
+            ToDoDatabase db = new ToDoDatabase(context.getApplicationContext());
+            PreferenceManager pm = new PreferenceManager(context, db);
+            RemoteViews views = new RemoteViews(context.getPackageName(), manager.getAppWidgetInfo(appWidgetId).initialLayout);
+    
+            views.setImageViewResource(R.id.widget_background, pm.getBackground());
+    
+            // set the note title
+            String title = db.getTitle(appWidgetId);
+            title.trim();
+            views.setTextViewText(R.id.notetitle, Html.fromHtml("<b><u>"+title+"</u></b>"));
+            views.setViewVisibility(R.id.notetitle, View.VISIBLE);
+            views.setTextColor(R.id.notetitle, pm.getActiveColor());
+            views.setFloat(R.id.notetitle, "setTextSize", pm.getTitleSize());
+            
+            if (title.length() == 0)
             {
-                noteField = R.id.class.getDeclaredField("note_"+(i+1)).getInt(null);
-                imageField = R.id.class.getDeclaredField("noteimage_"+(i+1)).getInt(null);
-                views.setViewVisibility(noteField, View.INVISIBLE);
-                views.setViewVisibility(imageField, View.INVISIBLE);
-                
-                views.setFloat(noteField, "setTextSize", pm.getSize());
-                
-                if (i >= notes.size()) { continue; }
-                
-                Note n = notes.get(i);
-                if (n.text != null && !n.text.equals(""))
+                views.setViewVisibility(R.id.notetitle, View.GONE);
+            }
+            
+            // set the note items
+            LinkedList<Note> notes = db.getAllNotes(appWidgetId);
+            
+            db.close();
+            db = null;
+    
+            int noteField;
+            int imageField;
+            
+            for (int i=0; i< MAX_NOTES; i++)
+            {
+                try
                 {
-                    views.setViewVisibility(noteField, View.VISIBLE);
-                    views.setViewVisibility(imageField, View.VISIBLE);
-                    if (pm.isEmptyIcon()) views.setViewVisibility(imageField, View.GONE);
-                    int imageDrawable = pm.getActiveIcon();
-                    if (n.status == Note.Status.FINISHED) imageDrawable = pm.getFinishedIcon();
-                    views.setImageViewResource(imageField, imageDrawable);
-                    int textColor = pm.getActiveColor();
-                    if (n.status == Note.Status.FINISHED) textColor = pm.getFinishedColor();
-                    views.setTextViewText(noteField, n.text);
-                    views.setTextColor(noteField, textColor);
+                    noteField = R.id.class.getDeclaredField("note_"+(i+1)).getInt(null);
+                    imageField = R.id.class.getDeclaredField("noteimage_"+(i+1)).getInt(null);
+                    views.setViewVisibility(noteField, View.INVISIBLE);
+                    views.setViewVisibility(imageField, View.INVISIBLE);
+                    
+                    views.setFloat(noteField, "setTextSize", pm.getSize());
+                    
+                    if (i >= notes.size()) { continue; }
+                    
+                    Note n = notes.get(i);
+                    if (n.text != null && !n.text.equals(""))
+                    {
+                        views.setViewVisibility(noteField, View.VISIBLE);
+                        views.setViewVisibility(imageField, View.VISIBLE);
+                        if (pm.isEmptyIcon()) views.setViewVisibility(imageField, View.GONE);
+                        int imageDrawable = pm.getActiveIcon();
+                        if (n.status == Note.Status.FINISHED) imageDrawable = pm.getFinishedIcon();
+                        views.setImageViewResource(imageField, imageDrawable);
+                        int textColor = pm.getActiveColor();
+                        if (n.status == Note.Status.FINISHED) textColor = pm.getFinishedColor();
+                        views.setTextViewText(noteField, n.text);
+                        views.setTextColor(noteField, textColor);
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
                 }
             }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
+    
+            // Tell the AppWidgetManager to perform an update on the current App Widget
+            // Create an Intent to launch ToDoActivity
+            Intent intent = new Intent(context, ToDoActivity.class);
+            intent.setAction(appWidgetId+"");
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+    
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, intent, 0);
+            
+            views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+            
+            manager.updateAppWidget(appWidgetId, views);
         }
-
-        // Tell the AppWidgetManager to perform an update on the current App Widget
-        // Create an Intent to launch ToDoActivity
-        Intent intent = new Intent(context, ToDoActivity.class);
-        intent.setAction(appWidgetId+"");
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, appWidgetId, intent, 0);
-        
-        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
-        
-        manager.updateAppWidget(appWidgetId, views);
+        catch (Exception e) 
+        {
+            // had an error from user but nothing can be done.
+        }
     }
 }
